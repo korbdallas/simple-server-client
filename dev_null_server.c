69ca4c358b01f019addfc60be607a84ede5c5ba6
Michael Colombo <57098813+korbdallas@users.noreply.github.com>
2023-05-03 13:49:57 -0400

    added error handling for socket and bind

--- dev_null_server.c
+++ dev_null_server.c
@@ -24,7 +24,10 @@ int main(int argc, char const *argv[])
     char buffer[1024] = {0};
     
     // Create socket file descriptor
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
     
     // Set socket options
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
@@ -35,16 +38,18 @@ int main(int argc, char const *argv[])
     address.sin_port = htons(PORT);
     
     // Bind socket to address
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
     
     // Listen for incoming connections
     listen(server_fd, 3);
     
     // Accept incoming connection
     new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-
     
-    // Read from socket and write to /dev/null
+        // Read from socket and write to /dev/null
     while ((valread = read(new_socket, buffer, 1024)) > 0) {
         write(open("/dev/null", O_WRONLY), buffer, valread);
         memset(buffer, 0, 1024);
